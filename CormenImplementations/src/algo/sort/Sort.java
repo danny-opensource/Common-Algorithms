a8effeeafc85edc1205eed654184f547856a4e87
madan1988 <madan1988@gmail.com>
2014-04-13 13:53:50 +0530

    First Commit

---
+++ CormenImplementations/src/algo/sort/Sort.java
@@ -0,0 +1,11 @@
+package algo.sort;
+
+public interface Sort {
+
+	public void sort(int[] unsorted);
+
+	public void printUnSortedList(int[] unsorted);
+
+	public void printSortedList(int[] sorted);
+
+}
