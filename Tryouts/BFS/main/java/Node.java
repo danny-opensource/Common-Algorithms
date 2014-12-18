7d086c62059deb2143eda3902a870dc69c9b4df4
Madan Gopal <madan1988@gmail.com>
2014-12-18 14:50:20 -0600

    Create Node.java

---
+++ Tryouts/BFS/main/java/Node.java
@@ -0,0 +1,40 @@
+package main.java;
+public class Node {
+  
+  public Node[] child;
+  public int childCount;
+  private String vertex;
+  public State state;
+  
+  public Node(String vertex)
+    {
+    this.vertex = vertex;
+  }
+  
+  public Node(String vertex, int children)
+    {
+    this.vertex = vertex;
+    childCount = 0;
+    child = new Node[children];
+  }
+  
+  public void addChildNode(Node adj)
+    {
+    adj.state = State.Unvisited;
+    if(childCount < 30)
+      {
+      this.child[childCount] = adj;
+      childCount++;
+    }
+  }
+  
+  public Node[] getChild()
+    {
+    return child;
+  }
+  
+  	public String getVertex()
+     {
+     return vertex;
+   }
+}
